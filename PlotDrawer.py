7935bc414e99a3f3f16da291fd038df02d2598c2
trinhphuson <imtrinhphuson1507@gmail.com>
2023-01-06 11:28:36 +0700

    Add main file

---
+++ PlotDrawer.py
@@ -0,0 +1,64 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+xvalv = input("Enter number of x to display (min:5, max:7): ")
+yvalv = input("Enter number of values to display (min:5, max:7): ")
+print("Make sure that both a's are in 0 for speed log.")
+print("x values:")
+if xvalv == '5':
+    a = input('a: ')
+    b = input('b: ') 
+    c = input('c: ')
+    d = input('d: ')
+    e = input('e: ')
+    x_values = [a, b, c, d, e]
+if xvalv == '6':
+    a = input('a: ')
+    b = input('b: ')
+    c = input('c: ')
+    d = input('d: ')
+    e = input('e: ')
+    f = input('f: ')
+    x_values = [a, b, c, d, e, f]
+if xvalv == '7':
+    a = input('a: ')
+    b = input('b: ')
+    c = input('c: ')
+    d = input('d: ')
+    e = input('e: ')
+    f = input('f: ')
+    g = input('g: ')
+    x_values = [a, b, c, d, e, f, g]
+print("Values:")
+if yvalv == '5':
+    a = input('a:')
+    b = input('b:')
+    c = input('c:')
+    d = input('e:')
+    e = input('f:')
+    squares = [a, b, c, d, e]
+if yvalv == '6':
+    a = input('a: ')
+    b = input('b: ')
+    c = input('c: ')
+    d = input('d: ')
+    e = input('e: ')
+    f = input('f: ')
+    squares = [a, b, c, d, e, f]
+if yvalv == '7':
+    a = input('a: ')
+    b = input('b: ')
+    c = input('c: ')
+    d = input('d: ')
+    e = input('e: ')
+    f = input('f: ')
+    g = input('g: ')
+    squares = [a, b, c, d, e, f ,g]
+title = input('Title: ')
+xlab = input('x label: ')
+ylab = input('y label: ')
+plt.title(title)
+plt.xlabel(xlab)
+plt.ylabel(ylab)
+plt.plot(x_values, squares)
+plt.show()
